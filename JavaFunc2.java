0e1e43a07acb0539777a61e5ddbf6af895741f76
Javed Kathat <javedkhankathat3040@gmail.com>
2024-04-10 18:49:49 +0530

    add Java e:; cd 'e:\JAVAZERO'; & 'C:\Program Files\Java\jdk-19\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\JavedKathat\AppData\Roaming\Code\User\workspaceStorage\62e84e79e2e256329e60da42ef15825b\redhat.java\jdt_ws\JAVAZERO_bee2e804\bin' 'JavaFunc2'

--- JavaFunc2.java
+++ JavaFunc2.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class JavaFunc2 {
+
+    public static void calculateFactorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println("factorial = " + fact);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number to calculate Factorial:-");
         int n = sc.nextInt();
+        calculateFactorial(n);
+        sc.close();
     }
 }
